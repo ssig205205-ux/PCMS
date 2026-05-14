289984fdc10be5c4ba417709775dafcf1a9e1ab2
min <ssig205205@gmail.com>
2026-05-14 21:13:34 +0630

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
-
 import MainPage from "./mainPage";
 import AddCusPage from "./addCuspage";
 import Detail from "./assets/component/detail";
 import Lead from "./lead";
-import {LoginPage,SignupPage} from "./assets/component/loginAndSigin";
+import { LoginPage, SignupPage } from "./assets/component/loginAndSigin";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {AuthProvider} from "./authContext.jsx";
+import { AuthProvider } from "./authContext.jsx";
 import ProtectedRoute from "./ProtectendRouth.jsx";
 import { DetailIdProvider } from "./assets/context/detail.jsx";
 import About from "./aboutPage.jsx";
+
 
 function App() {
   return (
     <>
       <AuthProvider>
         <DetailIdProvider>
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
-          <Route path="/add" element={<ProtectedRoute><AddCusPage /></ProtectedRoute>} />
-              <Route path="/detail" element={<ProtectedRoute><Detail /></ProtectedRoute>} />
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <MainPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/add"
+                element={
+                  <ProtectedRoute>
+                    <AddCusPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/detail"
+                element={
+                  <ProtectedRoute>
+                    <Detail />
+                  </ProtectedRoute>
+                }
+              />
               <Route path="/about" element={<About />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/lead" element={<ProtectedRoute><Lead /></ProtectedRoute>} />
-        </Routes>
-      </BrowserRouter>
-      </DetailIdProvider>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route
+                path="/lead"
+                element={
+                  <ProtectedRoute>
+                    <Lead />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </DetailIdProvider>
       </AuthProvider>
     </>
   );
--- src/loading.jsx
+++ src/loading.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export default function LoadingScreen() {
   return (
     <div className="loading-screen">
@@ -13,19 +13,4 @@ export default function LoadingScreen() {
     </div>
   );
 }
-=======
-export default function LoadingScreen() {
-  return (
-    <div className="loading-screen">
-      <div className="wifi-loader">
-        <div className="wifi-circle circle1"></div>
-        <div className="wifi-circle circle2"></div>
-        <div className="wifi-circle circle3"></div>
-        <div className="wifi-dot"></div>
-      </div>
 
-      <h2>Connecting...</h2>
-    </div>
-  );
-}
->>>>>>> 5e66926d0a38fe5a9c1991fcdd79f7eabc9a0e40
